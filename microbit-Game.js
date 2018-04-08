b995db066f4d7dd8580defd8ce8880241ad72749
shaan <shaanjassal101@gmail.com>
2018-04-08 19:27:10 +0100

    using state variable, and single on shake handler

--- microbit-Game.js
+++ microbit-Game.js
@@ -2,25 +2,71 @@ let degrees = 0
 let random = 0
 let direction = 0
 let num = 0
-function face_North() {
+let state = ""
+input.onGesture(Gesture.Shake, () => {
+    if (state == "is_north") {
+        instruct_random_drtn()
+    } else if (state == "qn_asked") {
+        qn_answered()
+    }
+})
+function face_North()  {
+    state = "face_north"
     num = 0
-    while (num == 0) {
+    while (state != "is_north") {
         degrees = input.compassHeading()
+        basic.showString("Face North")
         if (degrees > 315 || degrees <= 45) {
+            basic.showLeds(`
+                # . . . #
+                # # . . #
+                # . # . #
+                # . . # #
+                # . . . #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                # . . . #
+                # # . . #
+                # . # . #
+                # . . # #
+                # . . . #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                # . . . #
+                # # . . #
+                # . # . #
+                # . . # #
+                # . . . #
+                `)
             basic.showIcon(IconNames.Yes)
-            num = 1
+            state = "is_north"
         } else {
             basic.showIcon(IconNames.No)
         }
     }
 }
-input.onButtonPressed(Button.A, () => {
+function instruct_random_drtn()  {
     basic.clearScreen()
     direction = Math.random(3)
     if (random == 0) {
         basic.showString("Face East")
         if (degrees < 135) {
             basic.showIcon(IconNames.Happy)
+            basic.showString("?")
         } else {
             face_North()
         }
@@ -28,6 +74,7 @@ input.onButtonPressed(Button.A, () => {
         basic.showString("Face South")
         if (degrees < 225) {
             basic.showIcon(IconNames.Happy)
+            basic.showString("?")
         } else {
             face_North()
         }
@@ -35,12 +82,14 @@ input.onButtonPressed(Button.A, () => {
         basic.showString("Face West")
         if (degrees < 315) {
             basic.showIcon(IconNames.Happy)
+            basic.showString("?")
         } else {
             face_North()
         }
     }
-})
-input.onGesture(Gesture.Shake, () => {
+    state = "qn_asked"
+}
+function qn_answered()  {
     basic.clearScreen()
     random = Math.random(3)
     if (random == 2) {
@@ -50,6 +99,5 @@ input.onGesture(Gesture.Shake, () => {
     } else {
         basic.showString("MAYBE")
     }
-    face_North()
-})
+}
 face_North()
